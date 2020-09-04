57499ab6b08eca04b13695ddc7133603b52ee550
Jing <mqjing@gmail.com>
2020-09-04 18:48:00 +0800

    [add] stop button

--- src/01-play-mp3/client.js
+++ src/01-play-mp3/client.js
@@ -4,18 +4,34 @@ class Test{
     constructor() {
         this.context = null;
         this.buffer = null;
+        this.source = null;
     }
 
     initUI(){
-        let button = document.createElement('button');
-        button.innerHTML = 'ok';  // set the button content = "ok"
-        button.addEventListener('click', () => {
+        this.initBtPlay();
+        this.initBtStop();
+    }
+
+    initBtPlay(){
+        this.initBt('Play', () => {
             console.log('this.constructor.name = ', this.constructor.name); // Test
             console.log('clicked');
             this.initAudioContext();
             this.loadAudioBuffer("https://archive.org/download/100ClassicalMusicMasterpieces/1685%20Purcell%20,%20Trumpet%20Tune%20and%20Air.mp3");
-
+            // this.playSound(this.buffer);
         });
+    }
+
+    initBtStop(){
+        this.initBt('Stop', () => {
+            this.stopSound();
+        });
+    }
+
+    initBt(strTitle, listener) {
+        let button = document.createElement('button');
+        button.innerHTML = strTitle;  // set the button content
+        button.addEventListener('click', listener);
 
         var body = document.getElementsByTagName("body")[0];
         body.appendChild(button);
@@ -44,7 +60,7 @@ class Test{
             this.context.decodeAudioData(request.response, (theBuffer) => {
                 console.log('Get the data.')
                 this.buffer = theBuffer;
-                this.playSound(theBuffer);
+                this.playSound(this.buffer) 
                 console.log('buffer = ', this.buffer);
                 }, function () {
                     console.log('[Error] loadAudioBuffer:: decodeAudioData error.')
@@ -54,10 +70,23 @@ class Test{
     }
 
     playSound(buffer) {
-        var source = this.context.createBufferSource();
-        source.buffer = this.buffer;
-        source.connect(this.context.destination);
-        source.start(0);
+        this.source = this.context.createBufferSource();
+        this.source.buffer = this.buffer;
+        this.source.connect(this.context.destination);
+        this.source.start(0);
+    }
+
+    stopSound() {
+        if (this.source != null) {
+            this.source.stop();
+        }else{
+            this._showMessageError('[Error] stopSound:: this.source == null.');
+        }
+    }
+
+    _showMessageError(strMsg) {
+        console.log(strMsg);
+        alert(strMsg);
     }
 } // end of class
 
