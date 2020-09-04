0065e631abed8c2b73bacce78b6bcb635a47dfe2
jing-tw <mqjing@gmail.com>
2020-09-04 23:07:39 +0800

    [add] async promise

--- src/01-play-mp3/client.js
+++ src/01-play-mp3/client.js
@@ -8,27 +8,42 @@ class Test{
     }
 
     initUI(){
-        this.initBtPlay();
-        this.initBtStop();
+        this._initBtPlay();
+        this._initBtStop();
     }
 
-    initBtPlay(){
-        this.initBt('Play', () => {
+    _uiSoundPlay(buffer) {
+        this.source = this.context.createBufferSource();
+        this.source.buffer = this.buffer;
+        this.source.connect(this.context.destination);
+        this.source.start(0);
+    }
+
+    _uiSoundStop() {
+        if (this.source != null) {
+            this.source.stop();
+        }else{
+            this._showMessageError('[Error] _uiSoundStop:: this.source == null.');
+        }
+    }
+
+    _initBtPlay(){
+        this._initBtComm('Play', async () => {
             console.log('this.constructor.name = ', this.constructor.name); // Test
             console.log('clicked');
-            this.initAudioContext();
-            this.loadAudioBuffer("https://archive.org/download/100ClassicalMusicMasterpieces/1685%20Purcell%20,%20Trumpet%20Tune%20and%20Air.mp3");
-            // this.playSound(this.buffer);
+            this._initAudioContext();
+            this.buffer = await this._loadAudioBuffer("https://archive.org/download/100ClassicalMusicMasterpieces/1685%20Purcell%20,%20Trumpet%20Tune%20and%20Air.mp3");
+            this._uiSoundPlay(this.buffer);
         });
     }
 
-    initBtStop(){
-        this.initBt('Stop', () => {
-            this.stopSound();
+    _initBtStop(){
+        this._initBtComm('Stop', () => {
+            this._uiSoundStop();
         });
     }
 
-    initBt(strTitle, listener) {
+    _initBtComm(strTitle, listener) {
         let button = document.createElement('button');
         button.innerHTML = strTitle;  // set the button content
         button.addEventListener('click', listener);
@@ -37,7 +52,7 @@ class Test{
         body.appendChild(button);
     }
 
-    initAudioContext() {
+    _initAudioContext() {
         var contextClass = (window.AudioContext ||
             window.webkitAudioContext ||
             window.mozAudioContext ||
@@ -51,38 +66,26 @@ class Test{
         }
     }
 
-    loadAudioBuffer(url){
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.responseType = 'arraybuffer';
-        // Decode asynchronously
-        request.onload = () => {
-            this.context.decodeAudioData(request.response, (theBuffer) => {
-                console.log('Get the data.')
-                this.buffer = theBuffer;
-                this.playSound(this.buffer) 
-                console.log('buffer = ', this.buffer);
-                }, function () {
-                    console.log('[Error] loadAudioBuffer:: decodeAudioData error.')
-                });
+    _loadAudioBuffer(url){
+        return new Promise(resolve => {
+            var request = new XMLHttpRequest();
+            request.open('GET', url, true);
+            request.responseType = 'arraybuffer';
+            // Decode asynchronously
+            request.onload = () => {
+                this.context.decodeAudioData(request.response, (theBuffer) => {
+                    console.log('Get the data.')
+                    this.buffer = theBuffer;
+                    resolve(this.buffer);
+                    console.log('buffer = ', this.buffer);
+                    }, function () {
+                        console.log('[Error] _loadAudioBuffer:: decodeAudioData error.')
+                    });
+            } // end of onload
+            request.send();});
         }
-        request.send();
-    }
 
-    playSound(buffer) {
-        this.source = this.context.createBufferSource();
-        this.source.buffer = this.buffer;
-        this.source.connect(this.context.destination);
-        this.source.start(0);
-    }
-
-    stopSound() {
-        if (this.source != null) {
-            this.source.stop();
-        }else{
-            this._showMessageError('[Error] stopSound:: this.source == null.');
-        }
-    }
+    
 
     _showMessageError(strMsg) {
         console.log(strMsg);
